463d5511a2299858a3a73a3fd62de386250d79aa
Bohdan Shubenok <bohdan.shubenok@sigma.software>
2023-01-16 18:49:33 +0200

    WIP - investigate build issues(?)

--- src/utilities/nimble_npl_os.rs
+++ src/utilities/nimble_npl_os.rs
@@ -3,27 +3,11 @@ use esp_idf_sys::*;
 #[inline]
 #[allow(unused)]
 pub fn ble_npl_hw_enter_critical() {
-  #[cfg(esp32c3)]
-  unsafe {
-    vPortEnterCritical();
-  }
-
-  #[cfg(not(esp32c3))]
-  unsafe {
-    xPortEnterCriticalTimeout(&mut ble_port_mutex, portMUX_NO_TIMEOUT)
-  };
+  _ = npl_freertos_hw_enter_critical();
 }
 
 #[inline]
 #[allow(unused)]
 pub fn ble_npl_hw_exit_critical() {
-  #[cfg(esp32c3)]
-  unsafe {
-    vPortExitCritical();
-  }
-
-  #[cfg(not(esp32c3))]
-  unsafe {
-    vPortExitCritical(&mut ble_port_mutex)
-  };
+  _ = npl_freertos_hw_exit_critical(0);
 }
