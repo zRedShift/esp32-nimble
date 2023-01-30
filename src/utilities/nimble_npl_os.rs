5eb558e348f4b1c6ffc2549a6e22c0c7a0256c70
Bohdan Shubenok <bohdan.shubenok@sigma.software>
2023-01-30 14:47:07 +0200

    Fixup

--- src/utilities/nimble_npl_os.rs
+++ src/utilities/nimble_npl_os.rs
@@ -3,11 +3,15 @@ use esp_idf_sys::*;
 #[inline]
 #[allow(unused)]
 pub fn ble_npl_hw_enter_critical() {
-  _ = npl_freertos_hw_enter_critical();
+  unsafe {
+    _ = npl_freertos_hw_enter_critical();
+  }
 }
 
 #[inline]
 #[allow(unused)]
 pub fn ble_npl_hw_exit_critical() {
-  _ = npl_freertos_hw_exit_critical(0);
+  unsafe {
+    _ = npl_freertos_hw_exit_critical(0);
+  }
 }
